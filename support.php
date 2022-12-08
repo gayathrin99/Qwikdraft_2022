f349ce684d6b369bc81c718e7efaa28197aca8e2
gayathrin99 <118922038+gayathrin99@users.noreply.github.com>
2022-12-08 12:14:44 +0530

    adding page1 and support pages

---
+++ support.php
@@ -0,0 +1,37 @@
+<html>
+    <head>
+        <h1> Support </h1>
+        <style>
+            body{
+   font-family: 'Roboto',sans-serif, cursive;
+   background: #B6D0E2;
+   color: black;
+   text-align: center;
+}
+iframe{
+    display: block;
+    width: 500px;
+    height: 500px;
+    align: center;
+}
+</style>
+</head>
+<body>
+<a href="mailto: qwikdraft@gmail.com">
+    Mail us for any of your queries!
+</a>
+  <br>
+  <p> Call us on :+91-9964704397</p>
+  <p> Watch our demo video </p>
+  <br>
+          <p align="center">  
+          <iframe width="560" height="315" src="https://www.youtube.com/embed/iRyd3iZTkDc" title="Qwikdraft" frameborder="0" allowfullscreen></iframe>
+            </p>
+            <br>
+            <img src="gpayscan.jpeg" height="300" width="300" align="middle">
+<br>
+<p>Pay Rs.500 for drawings</p>
+<br>
+</img>
+            </body>
+            </html>
